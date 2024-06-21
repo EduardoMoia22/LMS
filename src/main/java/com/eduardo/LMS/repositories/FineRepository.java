b39c559f722d05db78f0d455b8d4b7cff5af1879
EduardoMoia22 <eduardomoiasantos@gmail.com>
2024-06-21 15:49:32 -0300

    removed repository annotation

--- src/main/java/com/eduardo/LMS/repositories/FineRepository.java
+++ src/main/java/com/eduardo/LMS/repositories/FineRepository.java
@@ -1,11 +1,9 @@
 package com.eduardo.LMS.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.eduardo.LMS.models.FineModel;
 
-@Repository()
 public interface FineRepository extends JpaRepository<FineModel, String> {
       
 }
--- src/main/java/com/eduardo/LMS/repositories/TransactionRepository.java
+++ src/main/java/com/eduardo/LMS/repositories/TransactionRepository.java
@@ -3,11 +3,9 @@ package com.eduardo.LMS.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.eduardo.LMS.models.TransactionModel;
 
-@Repository()
 public interface TransactionRepository extends JpaRepository<TransactionModel, String> {
       List<TransactionModel> findAllByUser(String userId);
 }
